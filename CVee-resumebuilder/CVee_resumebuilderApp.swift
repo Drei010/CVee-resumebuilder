b255ea8085a45d66c4365106448f7c201b70b3bc
Drei010 <8700andreihidalgo@gmail.com>
2026-07-22 11:35:52 +0800

    Initial Commit

---
+++ CVee-resumebuilder/CVee_resumebuilderApp.swift
@@ -0,0 +1,17 @@
+//
+//  CVee_resumebuilderApp.swift
+//  CVee-resumebuilder
+//
+//  Created by Andrei Kyle Hidalgo on 7/22/26.
+//
+
+import SwiftUI
+
+@main
+struct CVee_resumebuilderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
